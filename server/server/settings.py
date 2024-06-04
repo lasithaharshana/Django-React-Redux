acc8365628700579eaaf4cb723d904644070f3fd
Lasitha Harshana <lasitha6646@gmail.com>
2024-06-04 23:41:38 +0530

    add framewoek & mysql

--- server/server/settings.py
+++ server/server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "api",
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -74,11 +76,15 @@ WSGI_APPLICATION = "server.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'api_db_django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
-}
+} 
 
 
 # Password validation
